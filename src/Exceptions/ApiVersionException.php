3456c1025e350686a79bd0022fa6a54aaf7fc6ba
Knight Yoshi <6134576+KnightYoshi@users.noreply.github.com>
2026-03-22 22:49:25 -0400

    feat: add custom JSON response rendering for ApiVersionException

--- src/Exceptions/ApiVersionException.php
+++ src/Exceptions/ApiVersionException.php
@@ -2,7 +2,12 @@
 
 namespace GaiaTools\ContentAccord\Exceptions;
 
+use Illuminate\Http\JsonResponse;
+
 class ApiVersionException extends NegotiationException
 {
-    //
+    public function render(): JsonResponse
+    {
+        return new JsonResponse(['message' => $this->getMessage()], 406);
+    }
 }
